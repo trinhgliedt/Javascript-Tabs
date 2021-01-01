29f4761e060e2e616e2a80ce93c8a6b455e11c43
Trinh Gliedt <chuot2008@gmail.com>
2021-01-01 14:08:34 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+// import PersonCard from "./components/PersonCard";
+// import UserForm from "./components/HookForm";
+// import BoxList from "./components/BoxGenerator";
+import TabList from './components/Tabs';
+
+function App() {
+  return (
+    <div className="App">
+      <TabList/>
+      
+    </div>
+  );
+}
+
+export default App;
+
+
+
+{/* <PersonCard firstName="Jane" lastName="Doe" age={45} hairColor="Black"/>
+      <PersonCard firstName="John" lastName="Smith" age={88} hairColor="Brown"/>
+      <PersonCard firstName="Millard" lastName="Fillmore" age={50} hairColor="Brown"/>
+    <PersonCard firstName="Maria" lastName="Smith" age={62} hairColor="Brown"/> */}
+      
+      {/* <UserForm>
+      </UserForm> */}
+      {/* <BoxList>
+
+</BoxList> */}
+
+      
